930ede0c5a2d3f218da64a53da5a85ca10b18e79
Gogs <gogs@fake.local>
2023-12-16 10:21:17 +0200

    PIn1

--- PartyInvites/PartyInvites/Models/GuestResponse.cs
+++ PartyInvites/PartyInvites/Models/GuestResponse.cs
@@ -1,10 +1,16 @@
-﻿namespace PartyInvites.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PartyInvites.Models
 {
     public class GuestResponse
     {
+        [Required(ErrorMessage ="Введите ваше имя")]
         public string? Name {  get; set; }
+        [Required(ErrorMessage = "Введите ваш email")]
         public string? Email { get; set; }
+        [Required(ErrorMessage = "Введите ваш телефон")]
         public string? Phone { get; set; }
+        [Required(ErrorMessage = "Выберите согласны или нет")]
         public bool? WillAttend { get; set; }
     }
 }
